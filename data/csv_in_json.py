aadb550e42d473adb18cc1324c792da64793d70c
sacred70 <sacred70@mail.ru>
2023-05-13 23:56:47 +0700

    1

--- data/csv_in_json.py
+++ data/csv_in_json.py
@@ -11,6 +11,10 @@ def covert(csv_file, json_file, model):
                     row["is_published"] = True
                 else:
                     row["is_published"] = False
+            if "location_id" in row:
+                row["locations"] = [row["location_id"]]
+                del row["location_id"]
+
             result.append(row)
     with open(json_file, 'w', encoding="utf-8") as f:
         j = json.dumps(result, ensure_ascii=False)
@@ -18,5 +22,7 @@ def covert(csv_file, json_file, model):
 
 
 if __name__ == '__main__':
-    covert("ads.csv", "ads.json", "ads.ad")
-    covert("categories.csv", "categories.json", "ads.category")
+    covert("ad.csv", "ad.json", "ads.ad")
+    covert("category.csv", "category.json", "ads.category")
+    covert("user.csv", "user.json", "users.user")
+    covert("location.csv", "location.json", "users.location")
